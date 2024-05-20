345b775002623ed3fd49c46edc3e39f16e88eecf
TOUAOUA Elhabib <bibahhabib8@gmail.com>
2024-05-20 15:59:41 +0100

    Strings and Template Literals lecture watched and concept applied

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+/*
 const country = "Algeria";
 const continent = "Africa";
 let population = 45;
@@ -35,3 +35,55 @@ let description =
 	language;
 
 console.log(description);
+
+
+let massMark1 = 78,
+	heightMark1 = 1.69;
+let massJohn1 = 92,
+	heightJohn1 = 1.95;
+
+let massMark2 = 95,
+	heightMark2 = 1.88;
+let massJohn2 = 85,
+	heightJohn2 = 1.76;
+
+let BMIMark1 = massMark1 / (heightMark1 * heightMark1);
+let BMIJohn1 = massJohn1 / (heightJohn1 * heightJohn1);
+
+let BMIMark2 = massMark2 / (heightMark2 * heightMark2);
+let BMIJohn2 = massJohn2 / (heightJohn2 * heightJohn2);
+
+console.log(BMIMark1, BMIJohn1);
+console.log(BMIMark2, BMIJohn2);
+
+let markHigherBMI1 = BMIMark1 > BMIJohn1;
+let markHigherBMI2 = BMIMark2 > BMIJohn2;
+
+console.log(markHigherBMI1);
+console.log(markHigherBMI2);
+*/
+
+let firstName = "Elhabib";
+let lastName = "TOUAOUA";
+let birthday = 1996;
+const year = 2024;
+let job = "Computer Science Engineer";
+
+const elhabib =
+	"I'm " +
+	lastName +
+	" " +
+	firstName +
+	", a " +
+	(year - birthday) +
+	" years old " +
+	job +
+	"!";
+
+console.log(elhabib);
+
+let elhabib2 = `I'm ${lastName} ${firstName}, a ${
+	year - birthday
+} years old ${job}!`;
+
+console.log(elhabib2);
