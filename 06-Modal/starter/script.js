fb0f511a14777de5604b8f9ff8b4b0e65dad53d9
Elhabib TOUAOUA <bibahhabib8@gmail.com>
2024-06-04 11:58:46 +0100

    finishing model challenge and learned how to work with classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const showModalBtns = document.querySelectorAll('.show-modal');
+const closeModalBtn = document.querySelector('.close-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+const showModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+showModalBtns.forEach(btn => {
+  btn.addEventListener('click', showModal);
+});
+
+closeModalBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden'))
+    closeModal();
+});
